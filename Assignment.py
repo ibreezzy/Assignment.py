ed533780a926db9362429d4269291c9985a21908
ibreezzy <42194698+ibreezzy@users.noreply.github.com>
2018-08-09 21:10:11 +0100

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,8 @@
+name = input('What is yout name?: ')
+print('Hello,', name)
+age = eval(input('How old are you?: '))
+print(age)
+# Calculate when the user will be hundred years old
+c = 100 - age
+hundred = 2018 + c
+print('You will be hundred years old in', hundred)
